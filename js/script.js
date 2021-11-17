0ce16f491ffd859b299fcaeb36dba204a7685c35
dominici94 <mattiadominici94@gmail.com>
2021-11-17 20:59:35 +0100

    click caselle cambia colore

--- js/script.js
+++ js/script.js
@@ -12,7 +12,26 @@
 // L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata
 
 // creazione griglia di gioco esempio difficoltà 1
+// dopo posso creare una funzione con un parametro x che determina quante caselle creare in base a x.
 
-// for(let i = 0; i<100; i++){
+const container = document.querySelector('.container');
+console.log(container);
 
-// }
+for(let i = 1; i<=100; i++){
+    container.innerHTML += `
+    <div class="box">${i}</div>
+    `;
+}
+
+// assegno a una variabile box(js) un array di tutti gli elementi con classe box(html) 
+
+let box = document.getElementsByClassName('box');
+console.log(box);
+
+// algoritmo di prova per cambiare colore alla casella cliccata perchè scorrendo tutto l'array ad ogni click non è performante
+for(let i = 0; i<100; i++){
+    box[i].addEventListener('click', function(){
+        box[i].classList.add('color');
+        console.log(i+1);
+    });
+}
